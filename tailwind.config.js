f56e6186048cfc0d711d1d4fdce5b52bc9bc2db2
AlehandroSV <alehandrosantos99@gmail.com>
2023-09-13 19:25:37 -0300

    chore: Removendo plugin não utilizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -72,5 +72,5 @@ module.exports = {
     },
   },
   // eslint-disable-next-line no-undef
-  plugins: [require('tailwindcss-animate'), require('tailwind-scrollbar')],
+  plugins: [require('tailwindcss-animate')],
 };
